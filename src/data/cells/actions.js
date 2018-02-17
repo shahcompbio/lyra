4e79fe4d61372eb1f82ac7fcc3742962efc9ce17
Samantha Leung <-l>
2018-02-16 17:22:31 -0800

    Add incomplete actions file for cells. NOT WORKING

---
+++ src/data/cells/actions.js
@@ -0,0 +1,8 @@
+import * as segsActions from './segs/actions.js'
+import * as treeActions from './tree/actions.js'
+
+
+const { segTypes, ...segsActionCreators } = segsActions
+const { treeTypes, ... }
+
+export 
