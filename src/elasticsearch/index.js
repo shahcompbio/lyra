8cc27b20c2aeea2f251a14cc8514c0d6783a2205
Samantha Leung <-l>
2018-02-21 14:28:50 -0800

    Add credentials to querying request

--- src/elasticsearch/index.js
+++ src/elasticsearch/index.js
@@ -1,25 +1,25 @@
 /**
-* General Elasticsearch utils
-*/
+ * General Elasticsearch utils
+ */
 
 const config = {
-	HOST: "http://localhost:9200/",
-	INDEX: "htert_sc763_tree",
-	//INDEX: "htert_tree", // For testing
-	//INDEX: "tree_test",
-	SEARCH: "/_search"
-}
-
+  HOST: "",
+  INDEX: "htert_sc763_tree",
+  //INDEX: "htert_tree", // For testing
+  //INDEX: "tree_test",
+  SEARCH: "/_search"
+};
 
 export function fetchQuery(query, index) {
-	return fetch(config.HOST + index + config.SEARCH, {
-		    method: "POST",
-		    body: JSON.stringify(query),
-		    headers: {
-		    "Content-Type": "application/json"
-		  }
-  		}
-  	).then(response => response.json(),
-  			error => console.log("An error occured.", error)
-  	)
+  return fetch(config.HOST + index + config.SEARCH, {
+    method: "POST",
+    body: JSON.stringify(query),
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(
+    response => response.json(),
+    error => console.log("An error occured.", error)
+  );
 }
