9947b1e9491ccb98b7d51d3ddcd3f16b07ab83a4
Samantha Leung <-l>
2018-02-22 12:50:26 -0800

    Create ui sagas, root saga and reducer

--- src/store.js
+++ src/store.js
@@ -1,9 +1,23 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { rootSaga } from "./state/sagas/index.js";
-import reducer from "./state/reducers/index.js";
 
+import { combineReducers } from "redux";
 import data from "./data/reducer.js";
+import ui from "./treeCellscape/ui/reducer.js";
+
+import { all, fork } from "redux-saga/effects";
+import dataSagas from "./data/sagas.js";
+import uiSagas from "./treeCellscape/ui/sagas.js";
+
+const reducer = combineReducers({
+  data,
+  ui
+});
+
+function* rootSaga() {
+  yield all([fork(dataSagas), fork(uiSagas)]);
+}
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducer, applyMiddleware(sagaMiddleware));
