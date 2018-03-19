b286db62afc63b359ef47eb8022e4d43d46623ee
Samantha Leung <-l>
2018-03-19 14:07:48 -0700

    Add title to dashboard

--- src/main/components/Content/Content.js
+++ src/main/components/Content/Content.js
@@ -3,8 +3,29 @@ import { connect } from "react-redux";
 import { getSelectedAnalysis } from "./selectors.js";
 import TreeCellscape from "treeCellscape/TreeCellscape.js";
 
+import styled, { css } from "react-emotion";
+
 const Content = ({ selectedAnalysis }) =>
-  selectedAnalysis.id === null ? null : <TreeCellscape />;
+  selectedAnalysis.id === null ? null : (
+    <ContentDiv>
+      <Title>{selectedAnalysis.id}</Title>
+      <TreeCellscape />
+    </ContentDiv>
+  );
+
+const Title = styled("span")`
+  margin-top: 5px;
+  margin-bottom: 5px;
+  padding: 5px;
+  display: inline-block;
+`;
+
+const ContentDiv = styled("div")`
+  width: 90%;
+  margin-top: 5px;
+  text-align: left;
+  margin-left: 100px;
+`;
 
 const mapState = state => ({
   selectedAnalysis: getSelectedAnalysis(state)
