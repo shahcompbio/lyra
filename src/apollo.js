f5a34459c24f90951b7fb0e40126a32ebc253f82
redpanda-cat <sleung@bccrc.ca>
2018-06-13 14:57:07 -0700

    Create .env file for development server

--- src/apollo.js
+++ src/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
 
-const httpLink = "http://localhost:4000";
+const httpLink = process.env.REACT_APP_GRAPHQL_URL || "/graphql";
 
 const link = createHttpLink({
   uri: httpLink,
