a44022e29ccd845585a77a4ef5a9d022e8b30864
Spenca <svatrt@bccrc.ca>
2019-03-05 11:53:46 -0500

    Perform further code organization and commenting

--- src/main/components/Browse/Filters.js
+++ src/main/components/Browse/Filters.js
@@ -44,39 +44,20 @@ class Filters extends Component {
     };
   }
 
-  handleOptions(analyses, filter) {
-    // get the options that pertain to the current filter type
-    let filterOptions = analyses.map(analysis => analysis[filter]);
-    // discard duplicates
-    const seenOptions = new Set();
-    filterOptions = filterOptions.filter(option => {
-      let stringOption = JSON.stringify(option);
-      return seenOptions.has(stringOption)
-        ? false
-        : seenOptions.add(stringOption);
-    });
-    // if filter type is libraryId or sampleId, expand arrays into individual choices
-    if (Array.isArray(filterOptions[0]))
-      filterOptions = [].concat(...filterOptions);
-    // return options in a react-select friendly format
-    return filterOptions.map(option => {
-      return { label: option, value: option };
-    });
-  }
+  handleAnalysesChange = analyses => this.props.onAnalysesChange(analyses);
 
-  isWithinFilter = (analysis, filters) =>
-    Object.keys(filters).reduce(
-      (result, filter) =>
-        result && this.isFilterWithinAnalysis(analysis, filter, filters),
-      true
-    );
-
+  /* 
+  Is the filter null? (ie. not present in the current selection)   
+    If so, then return true to the chain of ands, having no effect on the overall expression
+    If not, is the information being filtered an array in the analysis (ie. libraryIds or sampleIds)?
+      If so, does that array contain the chosen libraryId or sampleId?
+        If so, return true
+        If not, return false  
+      If not, then check if the filter in the selection matches the data in the analysis in question
+        If so, return true
+        If not, return false
+  */
   isFilterWithinAnalysis = (analysis, filter, filters) =>
-    /* 
-    is the filter null? (ie. not present in the current selection)
-    If so, then add a true to the chain of ands, having no effect on the overall expression
-    If not, then check if the filter in the selection matches the data in the analysis in question
-    */
     filters[filter]
       ? Array.isArray(analysis[filter])
         ? [].concat(...analysis[filter]).includes(filters[filter].label)
@@ -87,6 +68,18 @@ class Filters extends Component {
         ? true
         : false
       : true;
+
+  isWithinFilter = (analysis, filters) =>
+    /*
+    reduce() has an initialValue of true
+    if a false is added, the bool return will evaluate to false
+    otherwise, it will evaluate to true
+    */
+    Object.keys(filters).reduce(
+      (result, filter) =>
+        result && this.isFilterWithinAnalysis(analysis, filter, filters),
+      true
+    );
 
   handleFilterChange = name => value => {
     this.setState(
@@ -110,7 +103,43 @@ class Filters extends Component {
     );
   };
 
-  handleAnalysesChange = analyses => this.props.onAnalysesChange(analyses);
+  handleOptions = (analyses, filter) => {
+    // get the options that pertain to the current filter type
+    let filterOptions = analyses.map(analysis => analysis[filter]);
+    // discard duplicates
+    const seenOptions = new Set();
+    filterOptions = filterOptions.filter(option => {
+      let stringOption = JSON.stringify(option);
+      return seenOptions.has(stringOption)
+        ? false
+        : seenOptions.add(stringOption);
+    });
+    // if filter type is libraryId or sampleId, expand arrays into individual choices
+    if (Array.isArray(filterOptions[0]))
+      filterOptions = [].concat(...filterOptions);
+    // return options in a react-select friendly format
+    return filterOptions.map(option => {
+      return { label: option, value: option };
+    });
+  };
+
+  renderFilters = (classes, filters, analyses) =>
+    Object.keys(filters).map(filter => {
+      return (
+        <div key={filters[filter].label}>
+          <span>{filters[filter].label}</span>
+          <Select
+            classes={classes}
+            options={this.handleOptions(analyses, filter)}
+            components={makeAnimated}
+            value={this.state.chosenFilters[filter]}
+            onChange={this.handleFilterChange(String(filter))}
+            placeholder="Select..."
+            isClearable
+          />
+        </div>
+      );
+    });
 
   clearFilters = () =>
     this.setState(
@@ -128,49 +157,26 @@ class Filters extends Component {
       () => this.handleAnalysesChange(this.state.analyses)
     );
 
+  renderClearFiltersButton = () => (
+    <button
+      type="button"
+      className="clearButton"
+      onClick={() => this.clearFilters()}
+    >
+      Clear
+    </button>
+  );
+
   render() {
     const classes = this.props;
-
-    return (
-      <div className={classes.root}>
-        <div>{this.renderFilters()}</div>
-        <div>{this.renderClearFiltersButton()}</div>
-      </div>
-    );
-  }
-
-  renderFilters() {
-    const { classes } = this.props;
     const filters = this.filters;
     const analyses = this.state.analyses;
 
-    return Object.keys(filters).map(filter => {
-      return (
-        <div key={filters[filter].label}>
-          <span>{filters[filter].label}</span>
-          <Select
-            classes={classes}
-            options={this.handleOptions(analyses, filter)}
-            components={makeAnimated}
-            value={this.state.chosenFilters[filter]}
-            onChange={this.handleFilterChange(String(filter))}
-            placeholder="Select..."
-            isClearable
-          />
-        </div>
-      );
-    });
-  }
-
-  renderClearFiltersButton() {
     return (
-      <button
-        type="button"
-        className="clearButton"
-        onClick={() => this.clearFilters()}
-      >
-        Clear
-      </button>
+      <div className={classes.root}>
+        <div>{this.renderFilters(classes, filters, analyses)}</div>
+        <div>{this.renderClearFiltersButton()}</div>
+      </div>
     );
   }
 }
